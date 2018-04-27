f37dcd21d1007b4b79334fd6b7f60059d3b23753
Till Hoffmann <tillahoffmann@gmail.com>
2018-04-27 15:39:20 +0100

    Add long description.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import find_packages, setup
 
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+
 setup(
     name='openformat',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.3'
     ],
-    version="0.1",
+    version="0.1.1",
     author="Till Hoffmann",
+    long_descrition=long_description,
+    long_description_content_type="text/markdown",
 )
