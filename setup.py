d923ffb9ac19650d012e535db7cbeea921995eba
Till Hoffmann <tillahoffmann@gmail.com>
2018-05-02 11:31:42 +0100

    Fix Cameca's pixel coding by rolling the data.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         'numpy>=1.3'
     ],
-    version="0.1.2",
+    version="0.1.3",
     author="Till Hoffmann",
     long_description=long_description,
     long_description_content_type="text/markdown",
