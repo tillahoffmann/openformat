b2a436be5d6e5655a5769358e64d234d48f6ff7a
mburkittgray <mburkittgray@users.noreply.github.com>
2019-06-09 00:53:04 +0100

    Function to extract data, remove hot pixels, and align the frames of each image. Returns a dictionary containing the processed data and other relevant information. (#8)

--- tests/test_mims.py
+++ tests/test_mims.py
@@ -1,15 +1,20 @@
 import numpy as np
 from openformat.mims import load_mims, remove_hot_pixels, infer_translations, \
-    apply_translations, infer_hot_pixels, apply_mask
+    apply_translations, infer_hot_pixels, apply_mask, process_image_data
 
 FILENAME = 'tests/data/2018_04_25_Cd_AF33_1_ROI3.im'
 
+
+def test_process_image_data():
+    image_data = process_image_data(FILENAME, '12C 14N')
+    assert image_data['w'] == 256
+    assert image_data['translations'] is not None
+    assert 'SE' in image_data['detector_names']
 
 def test_load_mims():
     mims = load_mims(FILENAME)
     assert len(mims.tab_mass) == mims.mask_im.nb_mass
     assert mims.data.shape[1] == mims.mask_im.nb_mass
-
 
 def test_filter_align():
     padding = 8
@@ -18,6 +23,7 @@ def test_filter_align():
     translations = infer_translations(cleaned[:, 0], padding)
     aligned = apply_translations(cleaned, translations, padding)
     assert aligned.shape == cleaned.shape[:2] + (cleaned.shape[2] + 2 * padding,) * 2
+
 
 def test_apply_mask():
     mims = load_mims(FILENAME)
